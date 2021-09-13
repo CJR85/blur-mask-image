323e975e789af233cb26157277696cfb284f9b43
Calvin Roberts <roberts.calvin85@gmail.com>
2021-09-14 00:43:42 +0100

    Create function for blur effect on mouse move

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 'use-strict';
+let blurArea = document.getElementById('blur-area');
+
+document.onmousemove = function (e) {
+  blurArea.style.transform = `translate(${e.pageX}px, ${e.pageY}px)`;
+};
